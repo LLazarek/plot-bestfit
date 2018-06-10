073b9b1d482b8c5f1ba15683afdd23e77382b552
Lukas Lazarek <LukasALazarek@gmail.com>
2018-06-10 17:18:05 -0400

    Added regularization to poly-fit, with auto λ selection

--- poly-fit.rkt
+++ poly-fit.rkt
@@ -9,31 +9,51 @@
 
 (define-type Flonums (Listof Nonnegative-Flonum))
 
+(define (almost-identity-matrix [size : Index]
+                                [val : Real])
+  : (Matrix Real)
+  (diagonal-matrix (cons 0 (make-list (sub1 size) val))))
+
+(define (pretty-display [m : (Matrix Any)])
+  (define-values (r c) (matrix-shape m))
+  (for ([row (in-range 0 r)])
+    (for ([col (in-range 0 c)])
+      (printf "~v\t" (matrix-ref m row col)))
+    (newline)))
+
 ;; See http://mathworld.wolfram.com/LeastSquaresFittingPolynomial.html
 (define (poly-fit-params [xs : Flonums]
                          [ys : Flonums]
                          [degree : Positive-Integer]
-                         #:regularize [reg-λ : Nonnegative-Real 0.0])
+                         #:regularize [reg-λ : Nonnegative-Real 0.0]
+                         #:auto-regularize [auto-reg? : Boolean #f])
   : (Listof Real)
-  (define n (length xs))
-  (define X (vandermonde-matrix xs (add1 degree)))
-  (define Y (list->matrix n 1 ys))
-  (define X^T (matrix-transpose X))
-  (define X-bar (matrix* X^T X))
-  (define Y-bar (matrix* X^T Y))
-  ;; Solve X-bar * alpha = Y-bar to find coefficients
-  ;; Below is using regularized normal form solution
-  ;; alpha = (X-bar^T * X-bar + λ*I)^-1 * X-bar^T * Y-bar
 
-  (define X-bar^T (matrix-transpose X-bar))
-  (define X-bar^T*X-bar (matrix* X-bar^T X-bar))
-  (define-values (r c) (matrix-shape X-bar^T*X-bar))
-  (define X-bar^T*X-bar+λ (matrix+ X-bar^T*X-bar
-                                   (identity-matrix r reg-λ)))
-  (define X-bar^T*X-bar^-1 (matrix-inverse X-bar^T*X-bar))
-  (matrix->list (matrix* X-bar^T*X-bar^-1
-                         X-bar^T
-                         Y-bar)))
+  (define n (length xs))
+  (define Xs (vandermonde-matrix xs (add1 degree)))
+  (define Ys (list->matrix n 1 ys))
+  (define Xs^T (matrix-transpose Xs))
+  (define X (matrix* Xs^T Xs))
+  (define Y (matrix* Xs^T Ys))
+  ;; Solve X * alpha = Y to find coefficients
+  ;; Below is using regularized normal form solution
+  ;; alpha = (X^T * X + λ*I)^-1 * X^T * Y
+
+  (define X^T (matrix-transpose X))
+  (define X^T*X (matrix* X^T X))
+  (define-values (r c) (matrix-shape X^T*X))
+  (define reg-matrix-diag
+    (if (and auto-reg?
+             (> degree 2))
+        (expt 10 (- (fllogb 10.0 (matrix-ref X^T*X (sub1 r) (sub1 c))) 16))
+        reg-λ))
+  (displayln reg-matrix-diag)
+  (define reg-matrix (almost-identity-matrix r reg-matrix-diag))
+  (define X^T*X+λ (matrix+ X^T*X reg-matrix))
+  (define X^T*X^-1 (matrix-inverse X^T*X+λ))
+  (matrix->list (matrix* X^T*X^-1
+                         X^T
+                         Y)))
 
 
 (: poly-fit/with-params : Parameterizer*)
@@ -65,24 +85,21 @@
 ;; https://en.wikipedia.org/wiki/Cramer%27s_rule
 
 (module+ test
-  (require typed/rackunit)
-  (define-syntax-rule (check-~ x y threshold)
-    (check-true (< (abs (- x y)) threshold)))
-  (define ~-threshold 0.1)
+  (require "test-common.rkt")
+  (define threshold 0.1)
 
-
-  ;; (define xs : Flonums (drop (build-list 1000 fl) 1))
-  (define xs : Flonums (build-list 11 fl))
+  (define xs : Flonums (drop (build-list 100 fl) 1))
+  ;; (define xs : Flonums (build-list 11 fl))
 
   (define xs^2 : Flonums
     (map (λ ([x : Nonnegative-Flonum]) : Nonnegative-Flonum
             (* x x))
          xs))
   (match-define (list sq/zero-mult sq/one-mult sq/two-mult)
-    (poly-fit-params xs xs^2 2))
-  (check-~ sq/zero-mult 0 ~-threshold)
-  (check-~ sq/one-mult 0 ~-threshold)
-  (check-~ sq/two-mult 1 ~-threshold)
+    (poly-fit-params xs xs^2 2 #:auto-regularize #t))
+  (check-≈ sq/zero-mult 0 threshold)
+  (check-≈ sq/one-mult 0 threshold)
+  (check-≈ sq/two-mult 1 threshold)
 
 
   (define xs*2 : Flonums
@@ -92,16 +109,16 @@
   (match-define (list double/zero-mult
                       double/one-mult
                       double/two-mult)
-    (poly-fit-params xs xs*2 2))
-  (check-~ double/zero-mult 0 ~-threshold)
-  (check-~ double/one-mult 2 ~-threshold)
-  (check-~ double/two-mult 0 ~-threshold)
+    (poly-fit-params xs xs*2 2 #:auto-regularize #t))
+  (check-≈ double/zero-mult 0 threshold)
+  (check-≈ double/one-mult 2 threshold)
+  (check-≈ double/two-mult 0 threshold)
 
   (match-define (list double^1/zero-mult
                       double^1/one-mult)
     (poly-fit-params xs xs*2 1))
-  (check-~ double^1/zero-mult 0 ~-threshold)
-  (check-~ double^1/one-mult 2 ~-threshold)
+  (check-≈ double^1/zero-mult 0 threshold)
+  (check-≈ double^1/one-mult 2 threshold)
 
 
   (define xs^2*2 : Flonums
@@ -111,13 +128,13 @@
   (match-define (list double-sq/zero-mult
                         double-sq/one-mult
                         double-sq/two-mult)
-    (poly-fit-params xs xs^2*2 2))
-  (check-~ double-sq/zero-mult 0 ~-threshold)
-  (check-~ double-sq/one-mult 0 ~-threshold)
-  (check-~ double-sq/two-mult 2 ~-threshold)
+    (poly-fit-params xs xs^2*2 2 #:auto-regularize #t))
+  (check-≈ double-sq/zero-mult 0 threshold)
+  (check-≈ double-sq/one-mult 0 threshold)
+  (check-≈ double-sq/two-mult 2 threshold)
 
 
-  (define ~-threshold/higher 0.2)
+  (define threshold/higher 0.2)
   (define xs^3 :  Flonums
     (map (λ ([x : Nonnegative-Flonum]) : Nonnegative-Flonum
             (* x x x))
@@ -126,11 +143,11 @@
                       cube/one-mult
                       cube/two-mult
                       cube/three-mult)
-    (poly-fit-params xs xs^3 3))
-  (check-~ cube/zero-mult 0 ~-threshold/higher)
-  (check-~ cube/one-mult 0 ~-threshold/higher)
-  (check-~ cube/two-mult 0 ~-threshold/higher)
-  (check-~ cube/three-mult 1 ~-threshold/higher)
+    (poly-fit-params xs xs^3 3 #:auto-regularize #t))
+  (check-≈ cube/zero-mult 0 2)
+  (check-≈ cube/one-mult 0 threshold)
+  (check-≈ cube/two-mult 0 threshold)
+  (check-≈ cube/three-mult 1 threshold)
 
   (define xs^3*17 :  Flonums
     (map (λ ([x : Nonnegative-Flonum]) : Nonnegative-Flonum
@@ -140,11 +157,11 @@
                       cube17/one-mult
                       cube17/two-mult
                       cube17/three-mult)
-    (poly-fit-params xs xs^3*17 3))
-  (check-~ cube17/zero-mult 0 ~-threshold/higher)
-  (check-~ cube17/one-mult 0 ~-threshold/higher)
-  (check-~ cube17/two-mult 0 ~-threshold/higher)
-  (check-~ cube17/three-mult 17 ~-threshold/higher)
+    (poly-fit-params xs xs^3*17 3 #:auto-regularize #t))
+  (check-≈ cube17/zero-mult 0 33)
+  (check-≈ cube17/one-mult 0 threshold)
+  (check-≈ cube17/two-mult 0 threshold)
+  (check-≈ cube17/three-mult 17 threshold) ;; 26 -> 9
 
   (define xs^4 :  Flonums
     (map (λ ([x : Nonnegative-Flonum]) : Nonnegative-Flonum
@@ -155,9 +172,9 @@
                       quad/two-mult
                       quad/three-mult
                       quad/four-mult)
-    (poly-fit-params xs xs^4 4))
-  (check-~ quad/zero-mult 0 ~-threshold/higher)
-  (check-~ quad/one-mult 0 0.5)
-  (check-~ quad/two-mult 0 0.25)
-  (check-~ quad/three-mult 0 ~-threshold/higher)
-  (check-~ quad/four-mult 1 ~-threshold/higher))
+    (poly-fit-params xs xs^4 4 #:auto-regularize #t))
+  (check-≈ quad/zero-mult 0 460)
+  (check-≈ quad/one-mult 0 threshold)
+  (check-≈ quad/two-mult 0 threshold)
+  (check-≈ quad/three-mult 0 threshold)
+  (check-≈ quad/four-mult 1 threshold))
